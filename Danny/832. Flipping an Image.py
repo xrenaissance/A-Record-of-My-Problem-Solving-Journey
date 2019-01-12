ab82a8ae023f022fba3f53c4f0a8dd204533fc2e
eastrd <yjia285@student.monash.edu>
2019-01-12 17:11:22 +1100

    20.10%

--- Danny/832. Flipping an Image.py
+++ Danny/832. Flipping an Image.py
@@ -4,13 +4,7 @@ class Solution:
         :type A: List[List[int]]
         :rtype: List[List[int]]
         """
-        for aIdx, col in enumerate(A):
-            for bIdx, e in enumerate(col):
-                # Reverse each cell
-                if e == 0:
-                    A[aIdx][bIdx] = 1
-                else:
-                    A[aIdx][bIdx] = 0
-            # Flip by reversing the column elements
-            A[aIdx] = A[aIdx][::-1]
+        for col in A:
+            for idx in range((len(col) + 1) // 2):
+                col[idx], col[~idx] = col[~idx] ^ 1, col[idx] ^ 1
         return A
