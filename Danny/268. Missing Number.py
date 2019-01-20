2a7f3777119fabc9520e7e158a23fa942e85de85
eastrd <yjia285@student.monash.edu>
2019-01-20 21:29:14 +1100

    THIS IS SO STUNNING

--- Danny/268. Missing Number.py
+++ Danny/268. Missing Number.py
@@ -4,21 +4,12 @@ class Solution:
         :type nums: List[int]
         :rtype: int
         """
-        
-        if len(nums) == 1:
-            if 1 not in nums:
-                return 1
-        
-        # Sort O(NlogN)
-        sorted_nums = sorted(nums)
-        
-        if sorted_nums[0] != 0:
-            return 0
-        
+        res = len(nums)
         i = 0
-        while i < len(sorted_nums)-1:
-            #print(i)
-            if i + 1 != sorted_nums[i+1]:
-                break
+        print("initial res", res)
+        while i < len(nums):
+            res ^= i
+            res ^= nums[i]
             i += 1
-        return i + 1
+            print(res)
+        return res
