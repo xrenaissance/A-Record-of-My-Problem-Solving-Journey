ee18aa916c769a4e28d8556787d92fd76337aa5f
egbertli <egbert1121@gmail.com>
2019-02-28 20:04:39 +1100

    check circle

--- Egbert/Algorithm/Algorithm_Beginner/Class9/Check_If_Linked_List_Has_A_Cycle.java
+++ Egbert/Algorithm/Algorithm_Beginner/Class9/Check_If_Linked_List_Has_A_Cycle.java
@@ -8,24 +8,29 @@
  *   }
  * }
  */
-import java.util.HashMap;
+
+/**
+ * slow and faster pointers
+ * if there are A steps far away from slow and faster
+ * if no circle, it will go untill null
+ * otherwise, it will alway keep 1 steps regards the relative speed
+ * assume the circle length is B, the worst case is O(B), so time complexity is O(N)
+ * since there is no new object is created, so space complexity is O(1)
+ **/
 public class Solution {
   public boolean hasCycle(ListNode head) {
-    if (head == null) {
-      return false;
+    if (head == null || head.next == null) {
+      return false; 
     }
-    HashMap<ListNode, Integer> records = new HashMap<ListNode, Integer>();
-    records.put(head, 1);
-    ListNode curr = head;
-    while (curr != null) {
-      ListNode next = curr.next;
-      if (!records.containsKey(next)) {
-        records.put(next, 1);
-      } else {
-        return true;
+    ListNode slow = head, fast = head;
+    while (fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+      if (slow == fast) {
+        return true; 
       }
-      curr = next;
     }
     return false;
   }
 }
+
