e9c3151672ed79cb567a667a8ac9bcfe3df1c94a
egbertli <egbert1121@gmail.com>
2019-03-01 22:44:04 +1100

    more clean code

--- Egbert/Algorithm/Algorithm_Beginner/Class10/Classical_Binary_Search.java
+++ Egbert/Algorithm/Algorithm_Beginner/Class10/Classical_Binary_Search.java
@@ -1,26 +1,19 @@
 public class Solution {
   public int binarySearch(int[] array, int target) {
     if (array == null || array.length == 0) {
-      return -1; 
+      return -1;
     }
     int left = 0, right = array.length - 1;
-    while (left + 1 < right) {
+    while (left <= right) {
       int mid = left + (right - left) / 2;
       if (target == array[mid]) {
-        return mid;
-      } else if (target < array[mid]) {
-        right = mid;
+        return mid; 
+      } else if (target > array[mid]) {
+        left = mid + 1; 
       } else {
-         left = mid;
+         right = mid - 1; 
       }
     }
-    
-    if (array[left] == target) {
-      return left;
-    } else if (array[right] == target) {
-      return right;
-    } else {
-      return -1; 
-    }
+    return -1;
   }
 }
