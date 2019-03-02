48bcf135a7b28acc1861d6b729b3f3abc5d75f2c
egbertli <egbert1121@gmail.com>
2019-03-03 00:22:15 +1100

    more clean code

--- Egbert/Algorithm/Algorithm_Beginner/Class10/First_Occurrence.java
+++ Egbert/Algorithm/Algorithm_Beginner/Class10/First_Occurrence.java
@@ -1,24 +1,21 @@
 public class Solution {
   public int firstOccur(int[] array, int target) {
     if (array == null || array.length == 0) {
-      return -1;
+      return -1; 
     }
     int left = 0, right = array.length - 1;
-    while (left + 1 < right) {
+    while (left < right - 1) {
       int mid = left + (right - left) / 2;
-      if (target == array[mid]) {
+      if (target <= array[mid]) {
         right = mid; 
-      } else if (target > array[mid]) {
-        left = mid; 
       } else {
-        right = mid; 
+        left = mid;
       }
     }
-  
     if (array[left] == target) {
       return left; 
     } else if (array[right] == target) {
-      return right; 
+      return right;
     } else {
       return -1; 
     }
