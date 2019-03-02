1fa7f7ab2ca95397f989c0a0b7499d737cf46db3
bo1 <wanbo.mail@gmail.com>
2019-03-02 17:22:26 +1100

    finish and optimise

--- Bo/Project1337/src/main/courses/laioffer/class1/mergeSort.java
+++ Bo/Project1337/src/main/courses/laioffer/class1/mergeSort.java
@@ -1,9 +1,5 @@
 package main.courses.laioffer.class1;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class mergeSort {
     public static void main(String[] args) {
         int a[] = {1, -1231, 15, 6, 88, 991, 5, -666, 712, 67, 588};
@@ -14,46 +10,23 @@ public class mergeSort {
         }
     }
 
-    // TODO WIP
     public static int[] mergeSort(int[] a, int start, int end) {
-        // If size is small enough to return
-        if(end == start) {
-            int[] result = {a[start]};
-            return result;
-        }
-
-        // If further recursion is needed
+        if(end == start)
+            return new int[]{a[start]};
         else {
             int half = start + (end - start) / 2;
             int firstHalf[] = mergeSort(a, start, half);
             int secondHalf[] = mergeSort(a, half + 1, end);
             int merged[] = new int[end - start + 1];
-
-            int firstPos = 0;
-            int secondPos = 0;
-            int mergedPos = 0;
-
-            while(mergedPos != end - start) {
-                if(firstPos == firstHalf.length - 1) {
-                    merged[mergedPos] = secondHalf[secondPos];
-                    secondPos ++;
-                }
-                else if(secondPos == secondHalf.length - 1) {
-                    merged[mergedPos] = firstHalf[firstPos];
-                    firstPos ++;
-                }
-                else if(firstHalf[firstPos] >= secondHalf[secondPos]) {
-                    merged[mergedPos] = firstHalf[firstPos];
-                    firstPos ++;
-                } else {
-                    merged[mergedPos] = secondHalf[secondPos];
-                    secondPos ++;
-                }
+            int firstPos = 0, secondPos = 0, mergedPos = 0;
+            while(mergedPos != merged.length) {
+                if(secondPos == secondHalf.length || (firstPos < firstHalf.length && firstHalf[firstPos] <= secondHalf[secondPos]))
+                    merged[mergedPos] = firstHalf[firstPos ++];
+                else
+                    merged[mergedPos] = secondHalf[secondPos ++];
                 mergedPos ++;
             }
             return merged;
         }
-
-
     }
 }
