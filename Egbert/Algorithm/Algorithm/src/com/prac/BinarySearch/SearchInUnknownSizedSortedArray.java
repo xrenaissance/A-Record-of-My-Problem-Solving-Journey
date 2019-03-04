fa3e89b84e30273e7780056e0eacf4002d487bbd
egbertli <egbert1121@gmail.com>
2019-03-05 02:21:06 +1100

    tired, do it tmr

--- Egbert/Algorithm/Algorithm/src/com/prac/BinarySearch/SearchInUnknownSizedSortedArray.java
+++ Egbert/Algorithm/Algorithm/src/com/prac/BinarySearch/SearchInUnknownSizedSortedArray.java
@@ -1,4 +1,11 @@
 package com.prac.BinarySearch;
-
+import java.util.Dictionary;
+/**
+ * @author  Egbert Li
+ * @Date 5/03/2019
+ */
 public class SearchInUnknownSizedSortedArray {
+    public int search(Dictionary dict, int target) {
+        return 0;
+    }
 }
