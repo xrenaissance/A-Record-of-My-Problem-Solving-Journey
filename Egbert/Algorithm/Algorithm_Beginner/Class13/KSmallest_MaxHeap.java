bc09f8a8dff1dc62ccc1183fd8f339f01380cbac
egbertli <egbert1121@gmail.com>
2019-03-09 14:58:42 +1100

    more clean code

--- Egbert/Algorithm/Algorithm_Beginner/Class13/KSmallest_MaxHeap.java
+++ Egbert/Algorithm/Algorithm_Beginner/Class13/KSmallest_MaxHeap.java
@@ -3,27 +3,24 @@ public class Solution {
     if (array.length == 0 || k == 0) {
       return new int[0]; 
     }
-    PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(new Comparator<Integer>() {
-      @Override
+    PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(k, new Comparator<Integer>(){
       public int compare(Integer o1, Integer o2) {
         if (o1.equals(o2)) return 0;
         return o1 > o2 ? -1 : 1;
       }
     });
-    for (int i = 0; i < k; i++) {
-      maxHeap.offer(array[i]); 
-    }
-    for (int i = k; i < array.length; i++) {
-      if(maxHeap.peek() > array[i]) {
+    for (int i = 0; i < array.length; i++) {
+      if (i < k) {
+        maxHeap.offer(array[i]); 
+      } else if (maxHeap.peek() > array[i]) {
         maxHeap.poll();
         maxHeap.offer(array[i]);
       }
     }
     int[] result = new int[k];
-    int j = k - 1;
     while (!maxHeap.isEmpty()) {
-      result[j] = maxHeap.poll();
-      j--;
+      k--;
+      result[k] = maxHeap.poll();
     }
     return result;
   }
