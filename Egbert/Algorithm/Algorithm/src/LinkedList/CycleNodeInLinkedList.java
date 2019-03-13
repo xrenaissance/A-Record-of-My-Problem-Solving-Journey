53d5e1501d4fb4017798aa93513aadccbc5f53a0
egbertli <egbert1121@gmail.com>
2019-03-13 22:23:48 +1100

    slow and fast pointers, need to do mathematical induction again

---
+++ Egbert/Algorithm/Algorithm/src/LinkedList/CycleNodeInLinkedList.java
@@ -0,0 +1,27 @@
+package LinkedList;
+
+public class CycleNodeInLinkedList {
+    public ListNode cycleNode(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                break;
+            }
+        }
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+}
