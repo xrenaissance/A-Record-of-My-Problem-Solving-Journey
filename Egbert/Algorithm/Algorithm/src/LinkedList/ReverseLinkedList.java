bf8ee296f45d81f027a8789ba0bdd837782c9ab0
egbertli <egbert1121@gmail.com>
2019-03-16 00:38:27 +1100

    review, writting in both recursion and iteratiion

---
+++ Egbert/Algorithm/Algorithm/src/LinkedList/ReverseLinkedList.java
@@ -0,0 +1,27 @@
+package LinkedList;
+
+public class ReverseLinkedList {
+    public ListNode reverseRecursiion(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newNode = reverse(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newNode;
+    }
+
+    public ListNode reverseIteration(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode prev = null, curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
