a3b0261af58507cdc16d93297de58ee1c0a82dbd
egbertli <egbert1121@gmail.com>
2019-03-15 22:57:03 +1100

    write in recursion and iteration, the time complexity are all O(height), space complextiy is O(n)

--- Egbert/Algorithm/Algorithm/src/LinkedList/ReverseBinaryTreeUpsideDown.java
+++ Egbert/Algorithm/Algorithm/src/LinkedList/ReverseBinaryTreeUpsideDown.java
@@ -1,4 +1,37 @@
 package LinkedList;
 
 public class ReverseBinaryTreeUpsideDown {
+    public TreeNode reverseRecursion(TreeNode root) {
+        if (root == null || root.left == null) {
+            return root;
+        }
+        TreeNode newRoot = reverse(root.left);
+        root.left.left = root;
+        root.left.right = root.right;
+        root.left = null;
+        root.right = null;
+        return newRoot;
+    }
+
+    public TreeNode reverseIteration(TreeNode root) {
+        if (root == null || root.left == null) {
+            return root;
+        }
+        TreeNode curr = root, prev = null, prevRight = null;
+        while (curr != null) {
+            TreeNode next = curr.left;
+
+            // swap nodes
+            TreeNode currRight = curr.right;
+            curr.left = prev;
+            curr.right = prevRight;
+            prevRight = currRight;
+
+            // get prev then go to next
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
 }
