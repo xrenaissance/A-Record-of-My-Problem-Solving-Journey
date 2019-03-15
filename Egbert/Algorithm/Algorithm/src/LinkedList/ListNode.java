3dcb4b39ccb6edc2871795223f1ab0c89bc0e437
egbertli <egbert1121@gmail.com>
2019-03-15 22:55:22 +1100

    add ListNode and TreeNode class

---
+++ Egbert/Algorithm/Algorithm/src/LinkedList/ListNode.java
@@ -0,0 +1,10 @@
+package LinkedList;
+
+public class ListNode {
+     public int value;
+     public ListNode next;
+     public ListNode(int value) {
+         this.value = value;
+         next = null;
+     }
+}
---
+++ Egbert/Algorithm/Algorithm/src/LinkedList/TreeNode.java
@@ -0,0 +1,10 @@
+package LinkedList;
+
+public class TreeNode {
+    public int key;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int key) {
+        this.key = key;
+    }
+}
