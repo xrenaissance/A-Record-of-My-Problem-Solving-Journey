0a4bcc2c514aa1f92411386a9be388684528187d
egbertli <egbert1121@gmail.com>
2019-03-16 00:29:29 +1100

    slow and fast pointers

---
+++ Egbert/Algorithm/Algorithm/src/LinkedList/CheckIfLinkedListHasACycle.java
@@ -0,0 +1,18 @@
+package LinkedList;
+
+public class CheckIfLinkedListHasACycle {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
