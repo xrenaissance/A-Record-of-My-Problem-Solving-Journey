dff2aa1e4ab1a09d9250c3fb7d0746af7bf292e9
egbertli <egbert1121@gmail.com>
2019-03-16 23:59:01 +1100

    question class

---
+++ Egbert/Algorithm/Algorithm/src/LinkedList/RandomListNode.java
@@ -0,0 +1,10 @@
+package LinkedList;
+
+public class RandomListNode {
+    public int value;
+    public RandomListNode next;
+    public RandomListNode random;
+    public RandomListNode(int value) {
+      this.value = value;
+    }
+}
