5c826cb856e8b959d4581282aef9c479c5512250
egbertli <egbert1121@gmail.com>
2019-03-17 01:15:51 +1100

    start rush bs

---
+++ Egbert/Algorithm/Algorithm/src/BinarySearch/FirstOccurrence.java
@@ -0,0 +1,25 @@
+package BinarySearch;
+
+public class FirstOccurrence {
+    public int firstOccur(int[] array, int target) {
+        if (array == null || array.length == 0) {
+            return -1;
+        }
+        int left = 0, right = array.length - 1;
+        while (left < right - 1) {
+            int mid = left + (right - left) / 2;
+            if (target <= array[mid]) {
+                right = mid;
+            } else {
+                left = mid;
+            }
+        }
+        if (array[left] == target) {
+            return left;
+        } else if (array[right] == target) {
+            return right;
+        } else {
+            return -1;
+        }
+    }
+}
---
+++ Egbert/Algorithm/Algorithm/src/BinarySearch/LastOccurrence.java
@@ -0,0 +1,28 @@
+package BinarySearch;
+
+public class LastOccurrence {
+    public int lastOccur(int[] array, int target) {
+        if (array == null || array.length == 0) {
+            return -1;
+        }
+        int left = 0, right = array.length - 1;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (target == mid) {
+                left = mid;
+            } else if (target < array[mid]) {
+                right = mid;
+            } else {
+                left = mid;
+            }
+        }
+
+        if(target == array[right]) {
+            return right;
+        } else if (target == array[left]) {
+            return left;
+        } else {
+            return -1;
+        }
+    }
+}
