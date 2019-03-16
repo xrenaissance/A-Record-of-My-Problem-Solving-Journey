d3c81e65232dd1a084eae23d5d1fefa804839c7d
egbertli <egbert1121@gmail.com>
2019-03-16 23:57:36 +1100

    HashMap copy to clone, then mirroring

---
+++ Egbert/Algorithm/Algorithm/src/LinkedList/DeepCopyLinkedListWithRandomPointer.java
@@ -0,0 +1,26 @@
+package LinkedList;
+
+public class DeepCopyLinkedListWithRandomPointer {
+    public RandomListNode copy(RandomListNode head) {
+        if (head == null) {
+            return head;
+        }
+
+        Map<RandomListNode, RandomListNode> records = new HashMap<RandomListNode, RandomListNode>();
+        RandomListNode curr = head;
+
+        // start mirroring
+        while (curr != null) {
+            records.put(curr, new RandomListNode(curr.value));
+            curr = curr.next;
+        }
+        curr = head;
+        // load mirror
+        while (curr != null) {
+            records.get(curr).next = records.get(curr.next);
+            records.get(curr).random = records.get(curr.random);
+        }
+        // return mirror head
+        return records.get(head);
+    }
+}
