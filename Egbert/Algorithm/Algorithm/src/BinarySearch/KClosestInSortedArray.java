dcbc82048d0031e40a68cd74ec5ac0046c49b789
egbertli <egbert1121@gmail.com>
2019-03-18 17:56:00 +1100

    K closest version 1 with time complexity O(log n) + O(k)

--- Egbert/Algorithm/Algorithm/src/BinarySearch/KClosestInSortedArray.java
+++ Egbert/Algorithm/Algorithm/src/BinarySearch/KClosestInSortedArray.java
@@ -1,19 +1,38 @@
 package BinarySearch;
 import java.util.Arrays;
+
+/**
+ * @author  Egbert
+ * @Date 18/03/2019
+ */
+
 public class KClosestInSortedArray {
-    public int[] kClosestVersion1(int[] array, int target, int k) {
-        if (array == null || array.length == 0) {
+
+    
+    // The time complexity is O(log n) + k, space complexity is O(k)
+    public int[] kClosestVersion(int[] array, int target, int k) {
+        if (array == null || array.length == 0 || k == 0) {
             return new int[0];
         }
-        int left = 0, right = array.length - k - 1;
-        while (left < right) {
+        int left = 0, right = array.length - 1;
+        while (left < right - 1) {
             int mid = left + (right - left) / 2;
-            if (target - array[mid] > array[mid + k] - target) {
-                left = mid + 1;
-            } else {
+            if (target <= array[mid]) {
                 right = mid;
+            } else {
+                left = mid;
             }
         }
-        return Arrays.copyOfRange(array, left, k);
+        int[] result = new int[k];
+        for (int i = 0; i < k; i++) {
+            if (right >= array.length || left >= 0 && Math.abs(target - array[left]) <= Math.abs(target - array[right])) {
+                result[i] = array[left];
+                left--;
+            } else {
+                result[i] = array[right];
+                right++;
+            }
+        }
+        return result;
     }
 }
