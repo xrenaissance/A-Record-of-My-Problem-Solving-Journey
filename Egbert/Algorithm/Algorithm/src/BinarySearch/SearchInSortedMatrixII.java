8fca265d7f76e1077245978fd7c06e70540f4814
egbertli <egbert1121@gmail.com>
2019-03-18 22:22:55 +1100

    tricky, search from top right

--- Egbert/Algorithm/Algorithm/src/BinarySearch/SearchInSortedMatrixII.java
+++ Egbert/Algorithm/Algorithm/src/BinarySearch/SearchInSortedMatrixII.java
@@ -1,4 +1,29 @@
 package BinarySearch;
 
+/**
+ * @author Egbert Li
+ * @Date 18/03/2019
+ * Time Complexity O(m + n), Space Complexity O(m + n)
+ */
 public class SearchInSortedMatrixII {
+    public int[] search(int[][] matrix, int target) {
+        int[] result = new int[]{-1, -1};
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return result;
+        }
+        int row = matrix.length, col = matrix[0].length;
+        int rowIndex = 0, colIndex = col - 1;
+        while (rowIndex <= row - 1 && colIndex >= 0) {
+            if (target == matrix[rowIndex][colIndex]) {
+                result[0] = rowIndex;
+                result[1] = colIndex;
+                return result;
+            } else if (target > matrix[rowIndex][colIndex]) {
+                rowIndex++;
+            } else {
+                colIndex--;
+            }
+        }
+        return result;
+    }
 }
