198bacab11d082fbb49753d86146aaf5eaa4d11c
egbertli <egbert1121@gmail.com>
2019-03-20 00:40:52 +1100

    always compare to right,since left may not exist

---
+++ Egbert/Algorithm/Algorithm/src/BinarySearch/ShiftPosition.java
@@ -0,0 +1,22 @@
+package BinarySearch;
+
+public class ShiftPosition {
+    public int shiftPosition(int[] array) {
+        if (array == null || array.length == 0) {
+            return -1;
+        }
+        int left = 0, right = array.length - 1;
+        while (left < right - 1) {
+            int mid = left + (right - left) / 2;
+            if (array[mid] < array[right]) {
+                right = mid;
+            } else {
+                left = mid;
+            }
+        }
+        if (array[left] < array[right]) {
+            return left;
+        }
+        return right;
+    }
+}
