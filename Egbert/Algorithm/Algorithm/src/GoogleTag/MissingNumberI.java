cf82efb59debb92036f1f44fe8a59e0931ab7c56
egbertli <egbert1121@gmail.com>
2019-03-24 00:07:18 +1100

    add sum solution

--- Egbert/Algorithm/Algorithm/src/GoogleTag/MissingNumberI.java
+++ Egbert/Algorithm/Algorithm/src/GoogleTag/MissingNumberI.java
@@ -1,6 +1,7 @@
 package GoogleTag;
 
 public class MissingNumberI {
+    // using oxr operation to get missing number
     public int missingNumberSolution1(int[] nums) {
         int res = nums.length;
         for (int i = 0; i < nums.length; i++){
@@ -8,5 +9,16 @@ public class MissingNumberI {
             res ^= nums[i];
         }
         return res;
+    }
+
+    // using sum difference to get missing number
+    public int missingNumberSolution2(int[] nums) {
+        int sumArray = 0;
+        int sumLength = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sumArray += nums[i];
+            sumLength += i;
+        }
+        return sumLength + nums.length - sumArray;
     }
 }
