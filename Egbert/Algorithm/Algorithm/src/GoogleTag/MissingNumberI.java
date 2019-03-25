6ede23abe09e77b04df040828c0e63f49da76a5a
egbertli <egbert1121@gmail.com>
2019-03-26 02:03:34 +1100

    add xor solution

--- Egbert/Algorithm/Algorithm/src/GoogleTag/MissingNumberI.java
+++ Egbert/Algorithm/Algorithm/src/GoogleTag/MissingNumberI.java
@@ -3,9 +3,9 @@ package GoogleTag;
 public class MissingNumberI {
     // using oxr operation to get missing number
     public int missingNumberSolution1(int[] nums) {
-        int res = nums.length;
+        int res = nums.length + 1;
         for (int i = 0; i < nums.length; i++){
-            res ^= i;
+            res ^= (i + 1);
             res ^= nums[i];
         }
         return res;
