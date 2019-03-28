a3f812583ca0dfd4f1a13113435fb96c7e719b90
egbertli <egbert1121@gmail.com>
2019-03-28 19:10:44 +1100

    recursion

--- Egbert/Algorithm/Algorithm/src/LinkedList/ReverseLinkedListInPairsRecursion.java
+++ Egbert/Algorithm/Algorithm/src/LinkedList/ReverseLinkedListInPairsRecursion.java
@@ -4,14 +4,14 @@ package LinkedList;
  * @Date 28/03/2019 Second time
  */
 public class ReverseLinkedListInPairsRecursion {
-    public ListNode reverseInPairs(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
-        }
-        ListNode currNode = head.next;
-        ListNode newNode = reverseInPairs(currNode.next);
-        currNode.next = head;
-        head.next = newNode;
-        return currNode;
+   public ListNode reverseInPairs(ListNode head) {
+    if (head == null || head.next == null) {
+      return head;
     }
+    ListNode currNext = head.next;
+    ListNode newNode = reverseInPairs(currNext.next);
+    currNext.next = head;
+    head.next = newNode;
+    return currNext;
+  }
 }
