ff24e0eaa4b773118f0951f0d6cfc69bc8a0e165
egbertli <egbert1121@gmail.com>
2019-04-01 03:28:56 +1100

    update time complexity and space complexity

--- Egbert/Algorithm/Algorithm/src/Tree/InvertBinaryTree.java
+++ Egbert/Algorithm/Algorithm/src/Tree/InvertBinaryTree.java
@@ -1,5 +1,11 @@
 package Tree;
 
+/**
+ * @author Egbert Li
+ * @date 1/04/2019
+ * Time Complexity: O(n)
+ * Space Complexity: O(height)
+ */
 public class InvertBinaryTree {
     public TreeNode invertTree(TreeNode root) {
         if (root == null) {
