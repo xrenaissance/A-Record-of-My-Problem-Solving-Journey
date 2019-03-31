86ad141ac418e9d1b3585d5d2897502c62d99990
egbertli <egbert1121@gmail.com>
2019-03-31 23:21:02 +1100

    selection sort for linkedlist

--- Egbert/Algorithm/Algorithm/src/LinkedList/SelectionSortLinkedList.java
+++ Egbert/Algorithm/Algorithm/src/LinkedList/SelectionSortLinkedList.java
@@ -1,4 +1,58 @@
 package LinkedList;
 
+/**
+ * @author Egbert Li
+ * @date 31/03/2019
+ * Time Complexity: O(n ^ 2), Space Complexity: O(1)
+ */
 public class SelectionSortLinkedList {
+    public ListNode selectionSort(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode dummyNode = new ListNode(0);
+        ListNode dummy = dummyNode;
+        ListNode curr = head;
+
+        // O(2n) * n ---> O(n ^ 2)
+        while (curr != null) {
+            ListNode minNode = findMin(curr); // O(n)
+            dummy.next = minNode;
+            dummy = minNode;
+            curr = remove(curr, minNode); // O(n)
+        }
+        //dummy.next = curr;
+        return dummyNode.next;
+    }
+
+    // O(n)
+    private ListNode remove(ListNode head, ListNode target) {
+        ListNode dummyNode = new ListNode(0);
+        ListNode dummy = dummyNode;
+        ListNode curr = head;
+        while (curr != null) {
+            if (curr.value != target.value) {
+                dummy.next = curr;
+                dummy = curr;
+            } else {
+                dummy.next = curr.next;
+                break;
+            }
+            curr = curr.next;
+        }
+        return dummyNode.next;
+    }
+
+    // O(n)
+    private ListNode findMin(ListNode head) {
+        ListNode minNode = head;
+        ListNode curr = head;
+        while (curr != null) {
+            if (curr.value < minNode.value) {
+                minNode = curr;
+            }
+            curr = curr.next;
+        }
+        return minNode;
+    }
 }
