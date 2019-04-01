02edb915d5a0b90f2dbff221fa4816b4408cd1d0
egbertli <egbert1121@gmail.com>
2019-04-02 03:22:12 +1100

    bt

--- Egbert/Algorithm/Algorithm/src/Tree/BinaryTreeDiameter.java
+++ Egbert/Algorithm/Algorithm/src/Tree/BinaryTreeDiameter.java
@@ -1,4 +1,19 @@
 package Tree;
 
 public class BinaryTreeDiameter {
+    public int diameter(TreeNode root) {
+        if (root == null || root.left == null && root.right == null) {
+            return 0;
+        }
+        int diameter = height(root.left) + height(root.right) + 1;
+        int left = diameter(root.left);
+        int right = diameter(root.right);
+        return Math.max(diameter, Math.max(left, right));
+    }
+    private int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(height(root.left), height(root.right));
+    }
 }
