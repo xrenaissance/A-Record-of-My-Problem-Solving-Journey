008b183544dc8c9b290792e9d590e3f8f6ba9325
egbertli <egbert1121@gmail.com>
2019-04-02 17:41:05 +1100

    in-order iteartor

---
+++ Egbert/Algorithm/Algorithm/src/Tree/TreeIterator.java
@@ -0,0 +1,39 @@
+package Tree;
+import java.util.Deque;
+import java.util.ArrayDeque;
+
+/**
+ * In-order Iterator
+ * @author Egbert Li
+ * @date 2/04/2019
+ * Time Complexity: hasNext() --> O(1), next() worst: --> O(height) Average --> O(1)
+ * Space Complexity O(h)
+ */
+public class TreeIterator {
+    public Deque<TreeNode> stack;
+    public TreeIterator(TreeNode root) {
+        stack = new ArrayDeque<>();
+        fillStack(root);
+    }
+
+    // get next node value
+    public int next() {
+        TreeNode currNode = stack.pollLast();
+        fillStack(currNode.right);
+        return currNode.key;
+    }
+
+    // check stack
+    public boolean hasNext() {
+        return stack.isEmpty();
+    }
+
+    // push all the left to stack
+    public void fillStack (TreeNode root) {
+        while (root != null) {
+            stack.offerLast(root);
+            root = root.left;
+        }
+    }
+}
+
