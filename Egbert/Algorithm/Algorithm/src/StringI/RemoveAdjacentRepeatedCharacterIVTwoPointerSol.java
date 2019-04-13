e913d81670dd53fc9c8215d86c574afc048db7a6
egbertli <egbert1121@gmail.com>
2019-04-14 00:44:37 +1000

    using two pointers to simulate stack

---
+++ Egbert/Algorithm/Algorithm/src/StringI/RemoveAdjacentRepeatedCharacterIVTwoPointerSol.java
@@ -0,0 +1,34 @@
+package StringI;
+
+/**
+ * @author Egbert Li
+ * @date 13/04/2019
+ * Time: O(n)
+ * Space: O(1)
+ */
+public class RemoveAdjacentRepeatedCharacterIVTwoPointerSol {
+    public String deDup(String input) {
+        if (input == null || input.length() <= 1) {
+            return input;
+        }
+        char[] array = input.toCharArray();
+        int slow = 0;
+        int fast = 1;
+        while (fast < array.length) {
+            if (slow >= 0 && array[fast] == array[slow]) {
+                while (fast < array.length && array[fast] == array[slow]) {
+                    fast++;
+                }
+                slow--;
+            } else {
+                slow++;
+                array[slow] = array[fast];
+                fast++;
+            }
+        }
+        if (slow < 0) {
+            return "";
+        }
+        return new String(array, 0, slow + 1);
+    }
+}
