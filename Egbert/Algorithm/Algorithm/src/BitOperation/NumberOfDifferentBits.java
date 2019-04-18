01c8ff7a9feae75b6df6b8e417cf7ee5d28562dc
egbertli <egbert1121@gmail.com>
2019-04-19 02:56:38 +1000

    & >> >>> I know how to use

--- Egbert/Algorithm/Algorithm/src/BitOperation/NumberOfDifferentBits.java
+++ Egbert/Algorithm/Algorithm/src/BitOperation/NumberOfDifferentBits.java
@@ -1,4 +1,20 @@
 package BitOperation;
 
+/**
+ * @author Egbert Li
+ * @date 19/04/2019
+ * 15 >> 2 = -4   since >> use 0 to fill left
+ * -15 >>> = 60   >>> use 1 fill left
+ * Time: O(n)
+ * Space: O(1)
+ */
 public class NumberOfDifferentBits {
+    public int diffBits(int a, int b) {
+        int count = 0;
+        // start from a ^ b, count how many 1 in the bits representation
+        for (int i = a ^ b; i != 0; i = i >>> 1) {
+            count += (i & 1);
+        }
+        return count;
+    }
 }
