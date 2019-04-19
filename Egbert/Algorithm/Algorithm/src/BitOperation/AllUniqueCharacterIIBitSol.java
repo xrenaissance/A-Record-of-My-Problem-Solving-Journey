fee0b628ae84d8e43c22b29911b5549515b43e15
egbertli <egbert1121@gmail.com>
2019-04-19 18:22:36 +1000

    set kth bit to 1 and 0

--- Egbert/Algorithm/Algorithm/src/BitOperation/AllUniqueCharacterIIBitSol.java
+++ Egbert/Algorithm/Algorithm/src/BitOperation/AllUniqueCharacterIIBitSol.java
@@ -1,4 +1,27 @@
 package BitOperation;
 
+/**
+ * @author Egbert Li
+ * @date 19/04/2019
+ * Time: O(n)
+ * Space: O(1)
+ */
 public class AllUniqueCharacterIIBitSol {
+    public boolean allUnique(String word) {
+        if (word == null || word.length() == 0) {
+            return true;
+        }
+        int[] visited = new int[8];
+        for (int i = 0; i < word.length(); i++) {
+            char currLetter = word.charAt(i);
+            int row = currLetter / 32;
+            int col = currLetter % 32;
+            if ((visited[row] >> col & 1) == 1) {
+                return false;
+            } else {
+                visited[row] |= 1 << col;
+            }
+        }
+        return true;
+    }
 }
