d923b27925431618a108659963586d8dc8d15a40
egbertli <egbert1121@gmail.com>
2019-04-19 23:02:23 +1000

    same as II

--- Egbert/Algorithm/Algorithm/src/BitOperation/AllUniqueCharactersIBitSol.java
+++ Egbert/Algorithm/Algorithm/src/BitOperation/AllUniqueCharactersIBitSol.java
@@ -1,4 +1,25 @@
 package BitOperation;
 
+/**
+ * @author Egbert Li
+ * @date 19/04/2019
+ * Time: O(N)
+ * Space: O(1)
+ */
 public class AllUniqueCharactersIBitSol {
+    public boolean allUnique(String word) {
+        if (word == null || word.length() == 0) {
+            return true;
+        }
+        int record = 0;
+        for (int i = 0; i < word.length(); i++) {
+            int currPosition = word.charAt(i) - 'a';
+            if (((record >> currPosition) & 1) == 1) {
+                return false;
+            } else {
+                record |= (1 << currPosition);
+            }
+        }
+        return true;
+    }
 }
