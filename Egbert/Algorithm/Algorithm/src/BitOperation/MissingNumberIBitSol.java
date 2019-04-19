467d70bc89fe4ba29ef064f2444639df8cf609c0
egbertli <egbert1121@gmail.com>
2019-04-20 00:22:13 +1000

    bit xor

--- Egbert/Algorithm/Algorithm/src/BitOperation/MissingNumberIBitSol.java
+++ Egbert/Algorithm/Algorithm/src/BitOperation/MissingNumberIBitSol.java
@@ -1,4 +1,21 @@
 package BitOperation;
 
+/**
+ * @author Egbert Li
+ * @date 19/04/2019
+ * Time: O(N)
+ * Space: O(1)
+ */
 public class MissingNumberIBitSol {
+    public int missing(int[] array) {
+        if (array == null || array.length == 0) {
+            return 1;
+        }
+        int res = array.length + 1;
+        for (int i = 0; i < array.length; i++) {
+            res ^= (i + 1);
+            res ^= array[i];
+        }
+        return res;
+    }
 }
