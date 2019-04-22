db452d4140b47fc34949cb56b2688d5bd9dbb98e
egbertli <egbert1121@gmail.com>
2019-04-22 22:45:47 +1000

    more readable code, don't need to keep changing the value of records

--- Egbert/Algorithm/Algorithm/src/DP/MaxProductOfCuttingRope.java
+++ Egbert/Algorithm/Algorithm/src/DP/MaxProductOfCuttingRope.java
@@ -20,14 +20,15 @@ public class MaxProductOfCuttingRope {
         records[1] = 1;
         records[2] = 1;
         for (int i = 3; i < records.length; i++) {
+            int currMax = 0;
             // at least one of the partitions after cutting is <= i/2
             // so we can just pick that partition, and find the maximum product
             for (int j = 1; j <= i / 2; j++) {
                 // for the other partitions, we can choose not cutting it or
-                // cutting it, so the max number we can get is either i -j or records[i - j]
-                records[i] = Math.max(records[i],
-                        Math.max(j, records[j]) * Math.max(i - j, records[i - j]));
+                // cutting it, so the max number we can get is either i -j or array[i - j]
+                currMax = Math.max(currMax, Math.max(j, records[j]) * Math.max(i - j, records[i - j]));
             }
+            records[i] = currMax;
         }
         return records[length];
     }
