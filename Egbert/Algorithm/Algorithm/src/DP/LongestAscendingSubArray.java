ed37a5aa3f80cfc4f18268138eb53b339334d293
egbertli <egbert1121@gmail.com>
2019-04-22 16:47:04 +1000

    clean code with mathematical representation

--- Egbert/Algorithm/Algorithm/src/DP/LongestAscendingSubArray.java
+++ Egbert/Algorithm/Algorithm/src/DP/LongestAscendingSubArray.java
@@ -3,24 +3,25 @@ package DP;
 /**
  * @author Egbert Li
  * @date 20/04/2019
+ * States representation:
+ * M[i] = {i > 0 && i < n | M[i - 1] + 1 if array[i - 1] < array[i] else 1}
  * Time: O(N)
  * Space: O(N)
  */
 public class LongestAscendingSubArray {
-    // M[i] = {i > 0 && i < n | M[i - 1] + 1 if array[i - 1] < array[i] else 1}
     public int longest(int[] array) {
+        // Assumptions: the given array is not null
         if (array == null || array.length == 0) {
             return 0;
         }
-        int[] currLongest = new int[array.length + 1];
+        int curr = 1;
         int longest = 1;
-        currLongest[0] = 1;
-        for (int i = 1; i < currLongest.length - 1; i++) {
+        for (int i = 1; i < array.length; i++) {
             if (array[i] > array[i - 1]) {
-                currLongest[i] = currLongest[i - 1] + 1;
-                longest = Math.max(longest, currLongest[i]);
+                curr++;
+                longest = Math.max(longest, curr);
             } else {
-                currLongest[i] = 1;
+                curr = 1;
             }
         }
         return longest;
