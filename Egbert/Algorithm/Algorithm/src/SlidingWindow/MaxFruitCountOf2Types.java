075993383304bca699598d9bf4257f07cad0fc21
egbertli <egbert1121@gmail.com>
2019-06-17 03:39:07 +1000

    same

---
+++ Egbert/Algorithm/Algorithm/src/SlidingWindow/MaxFruitCountOf2Types.java
@@ -0,0 +1,31 @@
+package SlidingWindow;
+import java.util.Map;
+import java.util.HashMap;
+public class MaxFruitCountOf2Types {
+    public static int findLength(char[] arr) {
+        int windowStart = 0, maxLength = 0;
+        Map<Character, Integer> fruitFrequencyMap = new HashMap<>();
+        // try to extend the range [windowStart, windowEnd]
+        for (int windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+            fruitFrequencyMap.put(arr[windowEnd], fruitFrequencyMap.getOrDefault(arr[windowEnd], 0) + 1);
+            // shrink the sliding window, until we are left with '2' fruits in the frequency map
+            while (fruitFrequencyMap.size() > 2) {
+                fruitFrequencyMap.put(arr[windowStart], fruitFrequencyMap.get(arr[windowStart]) - 1);
+                if (fruitFrequencyMap.get(arr[windowStart]) == 0) {
+                    fruitFrequencyMap.remove(arr[windowStart]);
+                }
+                windowStart++; // shrink the window
+            }
+            maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+        }
+
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Maximum number of fruits: " +
+                MaxFruitCountOf2Types.findLength(new char[] { 'A', 'B', 'C', 'A', 'C' }));
+        System.out.println("Maximum number of fruits: " +
+                MaxFruitCountOf2Types.findLength(new char[] { 'A', 'B', 'C', 'B', 'B', 'C' }));
+    }
+}
