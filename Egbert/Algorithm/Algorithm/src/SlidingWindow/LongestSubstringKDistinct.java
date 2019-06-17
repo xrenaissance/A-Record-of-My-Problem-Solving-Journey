381b34407a5a7413228984ab3844ad7944a8da8a
egbertli <egbert1121@gmail.com>
2019-06-17 23:46:31 +1000

    same as k distinct letter

--- Egbert/Algorithm/Algorithm/src/SlidingWindow/LongestSubstringKDistinct.java
+++ Egbert/Algorithm/Algorithm/src/SlidingWindow/LongestSubstringKDistinct.java
@@ -1,4 +1,40 @@
 package SlidingWindow;
 
+import java.util.HashMap;
+import java.util.Map;
+/**
+ * @author Egbert Li
+ * @date 15/06/2019
+ * @Time O (n + n)
+ * @space O(k + 1) at most
+ */
 public class LongestSubstringKDistinct {
+    public static int findLength(String str, int k) {
+        if (str == null || str.length() == 0) {
+            return 0;
+        }
+        int windowStart = 0;
+        int longest = 0;
+        Map<Character, Integer> charFrequencyMap = new HashMap<>();
+        for (int windowEnd = 0; windowEnd < str.length(); windowEnd++) {
+            char rightChar = str.charAt(windowEnd);
+            charFrequencyMap.put(rightChar, charFrequencyMap.getOrDefault(rightChar, 0) + 1);
+            while (charFrequencyMap.size() > k) {
+                char leftChar = str.charAt(windowStart);
+                charFrequencyMap.put(leftChar, charFrequencyMap.get(leftChar) - 1);
+                if (charFrequencyMap.get(leftChar) == 0) {
+                    charFrequencyMap.remove(leftChar);
+                }
+                windowStart++;
+            }
+            longest = Math.max(longest, windowEnd - windowStart + 1);
+        }
+        return longest;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Length of the longest substring: " + LongestSubstringKDistinct.findLength("araaci", 2));
+        System.out.println("Length of the longest substring: " + LongestSubstringKDistinct.findLength("araaci", 1));
+        System.out.println("Length of the longest substring: " + LongestSubstringKDistinct.findLength("cbbebi", 3));
+    }
 }
