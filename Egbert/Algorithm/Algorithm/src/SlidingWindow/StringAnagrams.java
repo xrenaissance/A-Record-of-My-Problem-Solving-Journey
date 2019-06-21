b69dfb268517fef5fd458b63bda2fd0b6c7ebec9
egbertli <egbert1121@gmail.com>
2019-06-21 21:49:41 +1000

    same as string permutation

--- Egbert/Algorithm/Algorithm/src/SlidingWindow/StringAnagrams.java
+++ Egbert/Algorithm/Algorithm/src/SlidingWindow/StringAnagrams.java
@@ -1,4 +1,53 @@
 package SlidingWindow;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 
+/**
+ * @alink https://www.educative.io/collection/page/5668639101419520/5671464854355968/6005911854252032
+ * @author Egbert Li
+ * @date 20/06/2019
+ * @Time O M + N
+ * @Space O M
+ */
 public class StringAnagrams {
+    public static List<Integer> findStringAnagrams(String str, String pattern) {
+        int windowStart = 0, matched = 0;
+        Map<Character, Integer> charFrequencyMap = new HashMap<>();
+        for (char chr : pattern.toCharArray())
+            charFrequencyMap.put(chr, charFrequencyMap.getOrDefault(chr, 0) + 1);
+
+        List<Integer> resultIndices = new ArrayList<Integer>();
+        // our goal is to match all the characters from the map with the current window
+        for (int windowEnd = 0; windowEnd < str.length(); windowEnd++) {
+            char rightChar = str.charAt(windowEnd);
+            // decrement the frequency of the matched character
+            if (charFrequencyMap.containsKey(rightChar)) {
+                charFrequencyMap.put(rightChar, charFrequencyMap.get(rightChar) - 1);
+                if (charFrequencyMap.get(rightChar) == 0)
+                    matched++;
+            }
+
+            if (matched == charFrequencyMap.size()) // have we found an anagram?
+                resultIndices.add(windowStart);
+
+            if (windowEnd >= pattern.length() - 1) { // shrink the window
+                char leftChar = str.charAt(windowStart++);
+                if (charFrequencyMap.containsKey(leftChar)) {
+                    if (charFrequencyMap.get(leftChar) == 0)
+                        matched--; // before putting the character back, decrement the matched count
+                    // put the character back
+                    charFrequencyMap.put(leftChar, charFrequencyMap.get(leftChar) + 1);
+                }
+            }
+        }
+
+        return resultIndices;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(StringAnagrams.findStringAnagrams("ppqp", "pq"));
+        System.out.println(StringAnagrams.findStringAnagrams("abbcabc", "abc"));
+    }
 }
