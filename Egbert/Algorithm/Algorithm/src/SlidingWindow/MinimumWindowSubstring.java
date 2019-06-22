4f615e63d417733a9add2946d276491cb5023a3f
egbertli <egbert1121@gmail.com>
2019-06-22 17:45:52 +1000

    minimum substring

---
+++ Egbert/Algorithm/Algorithm/src/SlidingWindow/MinimumWindowSubstring.java
@@ -0,0 +1,52 @@
+package SlidingWindow;
+import java.util.*;
+
+/**
+ * @link https://www.educative.io/collection/page/5668639101419520/5671464854355968/4739274459054080
+ * @author Egbert Li
+ * @date 20/06/2019
+ * @Time O M + N
+ * @Space O M, worst case O N
+ */
+public class MinimumWindowSubstring {
+    public static String findPermutation(String str, String pattern) {
+        int windowStart = 0, matched = 0, minLength = str.length() + 1, subStrStart = 0;
+        Map<Character, Integer> charFrequencyMap = new HashMap<>();
+        for (char chr : pattern.toCharArray())
+            charFrequencyMap.put(chr, charFrequencyMap.getOrDefault(chr, 0) + 1);
+
+        // try to extend the range [windowStart, windowEnd]
+        for (int windowEnd = 0; windowEnd < str.length(); windowEnd++) {
+            char rightChar = str.charAt(windowEnd);
+            if (charFrequencyMap.containsKey(rightChar)) {
+                charFrequencyMap.put(rightChar, charFrequencyMap.get(rightChar) - 1);
+                if (charFrequencyMap.get(rightChar) = 0) // count every matching of a character
+                    matched++;
+            }
+
+            // shrink the window if we can, finish as soon as we remove a matched character
+            while (matched == pattern.length()) {
+                if (minLength > windowEnd - windowStart + 1) {
+                    minLength = windowEnd - windowStart + 1;
+                    subStrStart = windowStart;
+                }
+
+                char leftChar = str.charAt(windowStart++);
+                if (charFrequencyMap.containsKey(leftChar)) {
+                    // note that we could have redundant matching characters, therefore we'll decrement the
+                    // matched count only when the last occurrence of a matched character is going out of the window
+                    if (charFrequencyMap.get(leftChar) == 0)
+                        matched--;
+                    charFrequencyMap.put(leftChar, charFrequencyMap.get(leftChar) + 1);
+                }
+            }
+        }
+        return minLength > str.length() ? "" : str.substring(subStrStart, subStrStart + minLength);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(MinimumWindowSubstring.findPermutation("aabdec", "abc"));
+        System.out.println(MinimumWindowSubstring.findPermutation("abdabca", "abc"));
+        System.out.println(MinimumWindowSubstring.findPermutation("adcad", "abc"));
+    }
+}
