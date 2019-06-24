22d0339747929ca1d28db8ad95f8ec1e8ee38b8c
egbertli <egbert1121@gmail.com>
2019-06-24 17:07:14 +1000

    remove element

--- Egbert/AlgorithmTesting/src/TwoPointers/RemoveElement.java
+++ Egbert/AlgorithmTesting/src/TwoPointers/RemoveElement.java
@@ -1,4 +1,32 @@
 package TwoPointers;
 
+/**
+ * @link https://www.educative.io/collection/page/5668639101419520/5671464854355968/5070198568648704
+ * @author Egbert Li
+ * @date 23/06/2019
+ * @Time O N
+ * @Space O 1
+ */
 public class RemoveElement {
+    public static int remove(int[] arr, int key) {
+        if (arr == null || arr.length == 0) {
+            return -1;
+        }
+        int nextNonKeyIndex = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != key) {
+                arr[nextNonKeyIndex] = arr[i];
+                nextNonKeyIndex++;
+            }
+        }
+        return nextNonKeyIndex;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[] { 3, 2, 3, 6, 3, 10, 9, 3 };
+        System.out.println(RemoveElement.remove(arr, 3));
+
+        arr = new int[] { 2, 11, 2, 2, 1 };
+        System.out.println(RemoveElement.remove(arr, 2));
+    }
 }
