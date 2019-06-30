f533079e7d0507ed4174bc39fc6f72b2888c92d4
egbertli <egbert1121@gmail.com>
2019-07-01 03:35:24 +1000

    start reviewing for fast and slow pointers

---
+++ Egbert/AlgorithmTesting/src/FastSlowPointers/LinkedListCycle.java
@@ -0,0 +1,42 @@
+package FastSlowPointers;
+
+/**
+ * @link https://www.educative.io/collection/page/5668639101419520/5671464854355968/6556337280385024
+ * @author Egbert Li
+ * @date 1/07/2019
+ * @Time O N
+ * @Space O 1
+ */
+public class LinkedListCycle {
+    public static boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(6);
+        System.out.println("LinkedList has cycle: " + LinkedListCycle.hasCycle(head));
+
+        head.next.next.next.next.next.next = head.next.next;
+        System.out.println("LinkedList has cycle: " + LinkedListCycle.hasCycle(head));
+
+        head.next.next.next.next.next.next = head.next.next.next;
+        System.out.println("LinkedList has cycle: " + LinkedListCycle.hasCycle(head));
+    }
+}
---
+++ Egbert/AlgorithmTesting/src/FastSlowPointers/ListNode.java
@@ -0,0 +1,10 @@
+package FastSlowPointers;
+
+public class ListNode {
+    int value = 0;
+    ListNode next;
+
+    ListNode(int value) {
+        this.value = value;
+    }
+}
