249714dcbd551cf15909ce729ed1a26d20a398b6
egbertli <egbert1121@gmail.com>
2019-06-30 22:38:35 +1000

    update

--- Egbert/Algorithm/Algorithm/src/TwoPointers/QuadrupleSumToTarget.java
+++ Egbert/Algorithm/Algorithm/src/TwoPointers/QuadrupleSumToTarget.java
@@ -2,50 +2,62 @@ package TwoPointers;
 import java.util.*;
 
 /**
- * @link https://www.educative.io/collection/page/5668639101419520/5671464854355968/6210874538721280
- * @leetcode https://leetcode.com/problems/4sum/
+ * @link https://www.educative.io/collection/page/5668639101419520/5671464854355968/5796496094003200
  * @author Egbert Li
- * @date 27/06/2019
- * @Time O N * log N + N ^ 2
+ * @date 30/06
+ * @Time NlogN + N ^ 3
  * @Space O N
  */
-public class TripletSumZero {
-    public static List<List<Integer>> searchTriplets(int[] arr) {
-        List<List<Integer>> allTriplets = new ArrayList<>();
+public class QuadrupleSumToTarget {
+    public static List<List<Integer>> searchQuadruplets(int[] arr, int target) {
+        List<List<Integer>> quadruplets = new ArrayList<>();
         if (arr == null || arr.length == 0) {
-            return allTriplets;
+            return quadruplets;
         }
-        Arrays.sort(arr);
-        for (int i = 0; i < arr.length - 2; i++) {
+        Arrays.sort(arr); // Time:NlogN Space:O N
+        // N ^ 3
+        for (int i = 0; i < arr.length - 3; i++) {
             if (i > 0 && arr[i] == arr[i - 1]) {
                 continue;
             }
-            searchPair(arr, i + 1, -arr[i], allTriplets);
-        }
-        return allTriplets;
-    }
-    public static void searchPair(int[] arr, int left, int targetSum,
-                                  List<List<Integer>> allTriplets) {
-        int right = arr.length - 1;
-        while (left < right) {
-            int targetDiff = targetSum - arr[left];
-            if (targetDiff > arr[right]) {
-                left++;
-            } else if (targetDiff < arr[right]) {
-                right--;
-            } else {
-                allTriplets.add(Arrays.asList(-targetSum, arr[left++], arr[right--]));
-                while (left < right && arr[left] == arr[left - 1]) {
-                    left++;
+            // N ^ 2
+            for (int j = i + 1; j < arr.length - 2; j++) {
+                if (j > i + 1 && arr[j] == arr[j - 1]) {
+                    continue;
                 }
-                while (left < right && arr[right] == arr[right + 1]) {
-                    right--;
+                int targetSum = target - arr[i] - arr[j];
+                searchPairs(arr, i, j, targetSum, quadruplets);
+            }
+        }
+        return quadruplets;
+    }
+    private static void searchPairs(int[] arr, int first,
+                                    int second, int target,
+                                    List<List<Integer>> quadruplets) {
+        int tempSecond = second;
+        int start = second + 1;
+        int end = arr.length - 1;
+        while (start < end) {
+            int leftDiff = target - arr[start];
+            if (leftDiff < arr[end]) {
+                end--;
+            } else if (leftDiff > arr[end]) {
+                start++;
+            } else {
+                quadruplets.add(Arrays.asList(arr[first],
+                        arr[tempSecond], arr[start++], arr[end--]));
+                while (start < end && arr[start] == arr[start - 1]) {
+                    start++;
+                }
+                while (start < end && arr[end] == arr[end + 1]) {
+                    end--;
                 }
             }
         }
     }
     public static void main(String[] args) {
-        System.out.println(TripletSumZero.searchTriplets(new int[] { -3, 0, 1, 2, -1, 1, -2 }));
-        System.out.println(TripletSumZero.searchTriplets(new int[] { -5, 2, -1, -2, 3 }));
+        // -3, -1, 1, 1,
+        System.out.println(QuadrupleSumToTarget.searchQuadruplets(new int[] { 4, 1, 2, -1, 1, -3 }, 1));
+        System.out.println(QuadrupleSumToTarget.searchQuadruplets(new int[] { 2, 0, -1, 1, -2, 2 }, 2));
     }
 }
