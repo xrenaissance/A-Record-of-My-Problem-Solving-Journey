26f7008083c23744752232cc6f7cec38bd1c8f59
egbertli <egbert1121@gmail.com>
2019-07-02 22:07:11 +1000

    keep going, easy one

--- Egbert/Algorithm/Algorithm/src/FastSlowPointers/MiddleOfLinkedList.java
+++ Egbert/Algorithm/Algorithm/src/FastSlowPointers/MiddleOfLinkedList.java
@@ -1,4 +1,38 @@
 package FastSlowPointers;
 
+/**
+ * @link https://www.educative.io/collection/page/5668639101419520/5671464854355968/6033606055034880
+ * @author Egbert Li
+ * @date 2/7/2019
+ * @Time O N
+ * @Spce O 1
+ */
 public class MiddleOfLinkedList {
+    public static ListNode findMiddle(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        System.out.println("Middle Node: " + MiddleOfLinkedList.findMiddle(head).value);
+
+        head.next.next.next.next.next = new ListNode(6);
+        System.out.println("Middle Node: " + MiddleOfLinkedList.findMiddle(head).value);
+
+        head.next.next.next.next.next.next = new ListNode(7);
+        System.out.println("Middle Node: " + MiddleOfLinkedList.findMiddle(head).value);
+    }
 }
