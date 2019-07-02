1a1e455b4e4446329858523d8a214da523a882c1
egbertli <egbert1121@gmail.com>
2019-07-02 20:38:57 +1000

    need to review how to analysis time complexity

--- Egbert/AlgorithmTesting/src/FastSlowPointers/HappyNumber.java
+++ Egbert/AlgorithmTesting/src/FastSlowPointers/HappyNumber.java
@@ -1,4 +1,34 @@
 package FastSlowPointers;
 
+/**
+ * @link https://www.educative.io/collection/page/5668639101419520/5671464854355968/5315930441646080
+ * @author Egbert Li
+ * @date 2/7/2019
+ * @Time log N
+ * @Space O 1
+ */
 public class HappyNumber {
+    public static boolean find(int num) {
+        int slow = num;
+        int fast = num;
+        do {
+            slow = findSquareSum(slow);
+            fast = findSquareSum(findSquareSum(fast));
+        } while (slow != fast);
+        return slow == 1;
+    }
+    private static int findSquareSum(int num) {
+        int digit = 0;
+        int sum = 0;
+        while (num > 0) {
+            digit = num % 10;
+            sum += digit * digit;
+            num /= 10;
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        System.out.println(HappyNumber.find(23));
+        System.out.println(HappyNumber.find(12));
+    }
 }
