2821cc378cd7928556d7542bc4f2056d2d8f911a
egbertli <egbert1121@gmail.com>
2019-07-06 17:14:00 +1000

    review

--- Egbert/AlgorithmTesting/src/InPlaceReversalOfaLinkedList/ReverseEveryKElements.java
+++ Egbert/AlgorithmTesting/src/InPlaceReversalOfaLinkedList/ReverseEveryKElements.java
@@ -1,8 +1,36 @@
 package InPlaceReversalOfaLinkedList;
 
+/**
+ * @leetcode https://leetcode.com/problems/reverse-nodes-in-k-group/submissions/
+ * @author Egbert Li
+ * @date 6/7/2019
+ * @Time O N
+ * @Space O 1
+ */
 public class ReverseEveryKElements {
     public static ListNode reverse(ListNode head, int k) {
-        return head;
+        if (head == null || k <= 1) {
+            return head;
+        }
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
+        ListNode dummy = dummyHead;
+        ListNode prev = head;
+        ListNode curr = findKth(head, k);
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = null;
+            ReverseLinkedList.reverse(prev);
+            prev.next = next;
+
+            dummy.next = curr;
+            dummy = prev;
+
+            prev = next;
+            curr = findKth(next, k);
+        }
+        dummy.next = ReverseLinkedList.reverse(prev);
+        return dummyHead.next;
     }
     private static ListNode findKth(ListNode head, int k) {
         while (head != null && k > 1) {
