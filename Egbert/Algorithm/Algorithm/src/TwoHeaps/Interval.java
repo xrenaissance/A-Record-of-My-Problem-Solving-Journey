b5d6bc8c368bfad18148d5c4599d42ee1b6d620f
egbertli <egbert1121@gmail.com>
2019-07-14 04:37:23 +1000

    two heaps

--- Egbert/Algorithm/Algorithm/src/TwoHeaps/Interval.java
+++ Egbert/Algorithm/Algorithm/src/TwoHeaps/Interval.java
@@ -1,4 +1,11 @@
 package TwoHeaps;
 
 public class Interval {
+    int start = 0;
+    int end = 0;
+
+    Interval(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
 }
