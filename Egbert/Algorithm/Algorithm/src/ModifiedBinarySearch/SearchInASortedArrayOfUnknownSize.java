8d20945b79f00dc804bd2193a53c1615da2c04a8
egbertli <egbert1121@gmail.com>
2019-07-15 01:51:53 +1000

    bs

--- Egbert/Algorithm/Algorithm/src/ModifiedBinarySearch/SearchInASortedArrayOfUnknownSize.java
+++ Egbert/Algorithm/Algorithm/src/ModifiedBinarySearch/SearchInASortedArrayOfUnknownSize.java
@@ -1,4 +1,28 @@
 package ModifiedBinarySearch;
 
+/**
+ * @leetcode https://leetcode.com/problems/search-in-a-sorted-array-of-unknown-size/
+ * @date 13/7/2019
+ * @Time log N
+ * @Space 1
+ */
 public class SearchInASortedArrayOfUnknownSize {
+    public int search(ArrayReader reader, int target) {
+        int right = 1;
+        while (target > reader.get(right)) {
+            right *= 2;
+        }
+        int left = right / 2;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (target > reader.get(mid)) {
+                left = mid + 1;
+            } else if (target < reader.get(mid)) {
+                right = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
 }
