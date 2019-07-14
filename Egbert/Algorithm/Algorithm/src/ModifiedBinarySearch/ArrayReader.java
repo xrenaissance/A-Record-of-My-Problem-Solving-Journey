fd70d15a6e5249c08df05e8bcda7c7ba04858341
egbertli <egbert1121@gmail.com>
2019-07-14 22:58:40 +1000

    aux class

---
+++ Egbert/Algorithm/Algorithm/src/ModifiedBinarySearch/ArrayReader.java
@@ -0,0 +1,15 @@
+package ModifiedBinarySearch;
+
+public class ArrayReader {
+    int[] arr;
+
+    ArrayReader(int[] arr) {
+        this.arr = arr;
+    }
+
+    public int get(int index) {
+        if (index >= arr.length)
+            return Integer.MAX_VALUE;
+        return arr[index];
+    }
+}
