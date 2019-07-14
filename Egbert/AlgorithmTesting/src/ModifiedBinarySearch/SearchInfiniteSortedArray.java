e51b72186c6b8e8cbc680ec743fa361bc121c839
egbertli <egbert1121@gmail.com>
2019-07-15 00:04:26 +1000

    add bs

--- Egbert/AlgorithmTesting/src/ModifiedBinarySearch/SearchInfiniteSortedArray.java
+++ Egbert/AlgorithmTesting/src/ModifiedBinarySearch/SearchInfiniteSortedArray.java
@@ -1,4 +1,16 @@
 package ModifiedBinarySearch;
 
 public class SearchInfiniteSortedArray {
+    public static int search(ArrayReader reader, int key) {
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        ArrayReader reader = new ArrayReader(new int[] { 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30 });
+        System.out.println(SearchInfiniteSortedArray.search(reader, 16));
+        System.out.println(SearchInfiniteSortedArray.search(reader, 11));
+        reader = new ArrayReader(new int[] { 1, 3, 8, 10, 15 });
+        System.out.println(SearchInfiniteSortedArray.search(reader, 15));
+        System.out.println(SearchInfiniteSortedArray.search(reader, 200));
+    }
 }
