19535b067187dbc036d980d98cae915bb307b2eb
egbertli <egbert1121@gmail.com>
2019-07-15 00:05:28 +1000

    bs

--- Egbert/Algorithm/Algorithm/src/ModifiedBinarySearch/FirstBadVersion.java
+++ Egbert/Algorithm/Algorithm/src/ModifiedBinarySearch/FirstBadVersion.java
@@ -1,4 +1,27 @@
 package ModifiedBinarySearch;
 
+/**
+ * @leetcode https://leetcode.com/problems/first-bad-version/
+ * @date 14/7/2019
+ * @Time Log N
+ * @Space O 1
+ */
 public class FirstBadVersion {
+    public static int firstBadVersion(int n) {
+        int start = 1;
+        int end = n;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (!isBadVersion(mid)) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        return start;
+    }
+
+    private static boolean isBadVersion(int n) {
+        return true;
+    }
 }
