e7664251d25e644f006595ba023113982b6e6b75
egbertli <egbert1121@gmail.com>
2019-07-15 19:01:06 +1000

    move both to get rid of duplicated nums

--- Egbert/AlgorithmTesting/src/ModifiedBinarySearch/FindMinimuminRotatedSortedArrayII.java
+++ Egbert/AlgorithmTesting/src/ModifiedBinarySearch/FindMinimuminRotatedSortedArrayII.java
@@ -1,4 +1,31 @@
 package ModifiedBinarySearch;
 
-public interface FindMinimuminRotatedSortedArrayII {
+/**
+ * @leetcode https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/
+ * @Date 15/7/2019
+ * @Time worst N, best log N
+ * @Space 1
+ */
+public class FindMinimuminRotatedSortedArrayII {
+    public int findMin(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return Integer.MIN_VALUE;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == nums[start] && nums[mid] == nums[end]) {
+                start++;
+                end--;
+            } else {
+                if (nums[mid] <= nums[end]) {
+                    end = mid;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return nums[start];
+    }
 }
