95cb1bb77f4c2a3e82a72b988fa46bc1fed57b2e
egbertli <egbert1121@gmail.com>
2019-07-15 16:22:02 +1000

    look at end to see which way will move

---
+++ Egbert/Algorithm/Algorithm/src/ModifiedBinarySearch/FindMinimumInRotatedSortedArray.java
@@ -0,0 +1,26 @@
+package ModifiedBinarySearch;
+
+/**
+ * @leetcode https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+ * @date 15/7/2019
+ * @Time log N
+ * @Space 1
+ */
+public class FindMinimumInRotatedSortedArray {
+    public int findMin(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] < nums[end]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return nums[start];
+    }
+}
