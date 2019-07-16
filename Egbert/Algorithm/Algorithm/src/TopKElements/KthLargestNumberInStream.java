e0e5715e8fe9c1733d63b7cfac47b9d3c42837ff
egbertli <egbert1121@gmail.com>
2019-07-17 02:29:19 +1000

    minHeap for top kth largets, real time algorithm

---
+++ Egbert/Algorithm/Algorithm/src/TopKElements/KthLargestNumberInStream.java
@@ -0,0 +1,48 @@
+package TopKElements;
+
+import java.util.PriorityQueue;
+
+/**
+ * @leetcode https://leetcode.com/problems/kth-largest-element-in-a-stream/
+ * @date 16/7/2019
+ * @Time add log N
+ * @Space k
+ */
+public class KthLargestNumberInStream {
+    public PriorityQueue<Integer> minHeap;
+    public final int k;
+    public KthLargestNumberInStream(int[] nums, int k) {
+        minHeap = new PriorityQueue<>();
+        this.k = k;
+        for (int num : nums) {
+            if (minHeap.size() < k) {
+                minHeap.offer(num);
+            } else {
+                if (num > minHeap.peek()) {
+                    minHeap.poll();
+                    minHeap.offer(num);
+                }
+            }
+        }
+    }
+
+    public int add(int num) {
+        if (minHeap.size() < k) {
+            minHeap.offer(num);
+        } else {
+            if (num > minHeap.peek()) {
+                minHeap.poll();
+                minHeap.offer(num);
+            }
+        }
+        return minHeap.peek();
+    }
+
+    public static void main(String[] args) {
+        int[] input = new int[] { 3, 1, 5, 12, 2, 11 };
+        KthLargestNumberInStream kthLargestNumber = new KthLargestNumberInStream(input, 4);
+        System.out.println("4th largest number is: " + kthLargestNumber.add(6));
+        System.out.println("4th largest number is: " + kthLargestNumber.add(13));
+        System.out.println("4th largest number is: " + kthLargestNumber.add(4));
+    }
+}
