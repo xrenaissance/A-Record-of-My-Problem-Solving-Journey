3e1c54c8b0c1ca80ed829b00f55cd42dff050256
egbertli <egbert1121@gmail.com>
2019-07-20 07:33:22 +1000

    should be two pointer qs

--- Egbert/Algorithm/Algorithm/src/TopKElements/SquareSum.java
+++ Egbert/Algorithm/Algorithm/src/TopKElements/SquareSum.java
@@ -1,8 +1,9 @@
 package TopKElements;
 
 /**
- * @date 17/7/2019
- * @Time log N + N
+ * @date 20/7/2019
+ * @leetcode https://leetcode.com/problems/squares-of-a-sorted-array/
+ * @Time  N
  * Space O N if result array is counted
  */
 public class SquareSum {
@@ -10,46 +11,21 @@ public class SquareSum {
         if (arr == null || arr.length == 0) {
             return new long[0];
         }
-        int index = largetstSmallerZero(arr);
-        int left = Math.max(0, index);
-        int right = Math.min(index + 1, arr.length - 1);
         long[] result = new long[arr.length];
-        for (int i = 0; i < arr.length; i++) {
-            if (left >= 0 && right < arr.length) {
-                long leftSquare = arr[left] * arr[left];
-                long rightSquare = arr[right] * arr[right];
-                if (leftSquare <= rightSquare) {
-                    result[i] = leftSquare;
-                    left--;
-                } else {
-                    result[i] = rightSquare;
-                    right++;
-                }
-            } else if (right < arr.length) {
-                result[i] = arr[right] * arr[right];
-                right++;
-            } else if (left >= 0) {
+        int left = 0;
+        int right = arr.length - 1;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (Math.abs(arr[left]) >= Math.abs(arr[right])) {
                 result[i] = arr[left] * arr[left];
-                left--;
+                left++;
+            } else {
+                result[i] = arr[right] * arr[right];
+                right--;
             }
         }
         return result;
     }
-    private static int largetstSmallerZero(int[] arr) {
-        int start = 0;
-        int end = arr.length - 1;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (arr[mid] == 0) {
-                return mid;
-            } else if (arr[mid] > 0) {
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
-        }
-        return start > 0 ? start - 1 : start;
-    }
+
     public static void main(String[] args) {
         int[] input1 = new int[] {-6, -3, -1, 2, 4, 5};
         int[] input2 = new int[] {-5, -4, -2, 0, 1};
