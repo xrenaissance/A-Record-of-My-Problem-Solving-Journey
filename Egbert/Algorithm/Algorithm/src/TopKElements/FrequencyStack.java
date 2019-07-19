231d231b9148554b4e6ddab1fce3d3fd48b43131
egbertli <egbert1121@gmail.com>
2019-07-19 23:59:14 +1000

    top k kinda

--- Egbert/Algorithm/Algorithm/src/TopKElements/FrequencyStack.java
+++ Egbert/Algorithm/Algorithm/src/TopKElements/FrequencyStack.java
@@ -1,4 +1,59 @@
 package TopKElements;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+/**
+ * @leetcode https://leetcode.com/problems/maximum-frequency-stack/
+ * @link https://www.educative.io/collection/page/5668639101419520/5671464854355968/6223867750121472
+ *
+ */
 public class FrequencyStack {
+    public PriorityQueue<Element> maxHeap = new PriorityQueue<>(
+            (e1, e2) -> {
+                if (e1.frequence != e2.frequence) {
+                    return e2.frequence - e1.frequence;
+                }
+                return e2.sequenceNumber - e1.sequenceNumber;
+            }
+    );
+    public Map<Integer, Integer> numFreq = new HashMap<>();
+    public int sequenceNumber;
+    public void push(int num) {
+        numFreq.put(num, numFreq.getOrDefault(num, 0) + 1);
+        maxHeap.offer(new Element(num, numFreq.get(num), this.sequenceNumber++));
+    }
+    public int pop() {
+        int num = maxHeap.poll().number;
+        if (numFreq.get(num) > 1) {
+            numFreq.put(num, numFreq.get(num) - 1);
+        } else {
+            numFreq.remove(num);
+        }
+        return num;
+    }
+    public static class Element {
+        int number;
+        int frequence;
+        int sequenceNumber;
+        public Element(int number, int frequence, int sequenceNumber) {
+            this.number = number;
+            this.frequence = frequence;
+            this.sequenceNumber = sequenceNumber;
+        }
+    }
+    public static void main(String[] args) {
+        FrequencyStack frequencyStack = new FrequencyStack();
+        frequencyStack.push(5);
+        frequencyStack.push(7);
+        frequencyStack.push(5);
+        frequencyStack.push(7);
+        frequencyStack.push(4);
+        frequencyStack.push(5);
+        System.out.println(frequencyStack.pop());
+        System.out.println(frequencyStack.pop());
+        System.out.println(frequencyStack.pop());
+        System.out.println(frequencyStack.pop());
+    }
 }
