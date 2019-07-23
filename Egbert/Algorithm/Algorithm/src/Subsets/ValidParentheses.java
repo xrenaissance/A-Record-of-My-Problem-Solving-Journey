298042b16cf87fac530264abde671dd6a8bd60bc
egbertli <egbert1121@gmail.com>
2019-07-24 05:18:42 +1000

    dfs

---
+++ Egbert/Algorithm/Algorithm/src/Subsets/ValidParentheses.java
@@ -0,0 +1,29 @@
+package Subsets;
+import java.util.Deque;
+import java.util.LinkedList;
+/**
+ * @leetcode https://leetcode.com/problems/valid-parentheses/
+ * @date 23/7/2019
+ * @Time N
+ * @Space N/2
+ */
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        if (s == null || s.length() == 0) {
+            return true;
+        }
+        Deque<Character> stack = new LinkedList<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.offerLast(')');
+            } else if (s.charAt(i) == '[') {
+                stack.offerLast(']');
+            } else if (s.charAt(i) == '{') {
+                stack.offerLast('}');
+            } else if (stack.isEmpty() || s.charAt(i) != stack.pollLast()) {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
