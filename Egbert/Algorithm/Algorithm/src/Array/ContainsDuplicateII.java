72e293da18981c9bc14a217072bde4df8ce3f152
egbertli <egbert1121@gmail.com>
2019-07-28 04:40:00 +1000

    update set solution

--- Egbert/Algorithm/Algorithm/src/Array/ContainsDuplicateII.java
+++ Egbert/Algorithm/Algorithm/src/Array/ContainsDuplicateII.java
@@ -1,15 +1,30 @@
 package Array;
-
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @leetcode https://leetcode.com/problems/contains-duplicate-ii/
  * @date 27/7/2019
- * @Time N ^ 2 worst
- * @Space N
+ * @Time N  worst
+ * @Space N min(N,k)
  */
 public class ContainsDuplicateII {
+    // using set
+    public boolean containsNearbyDuplicateSet(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (i > k) {
+                set.remove(nums[i - k - 1]);
+            }
+            if (!set.add(nums[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+    // Solution 2: HashMap solution
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         if (nums == null || nums.length == 0) {
             return false;
@@ -17,15 +32,9 @@ public class ContainsDuplicateII {
         Map<Integer, Integer> map = new HashMap<>();
         // N
         for (int i = 0; i < nums.length; i++) {
-            // N
-            if (!map.containsKey(nums[i])) {
-                map.put(nums[i], i);
-            } else {
-                if (i - map.get(nums[i]) <= k) {
-                    return true;
-                } else {
-                    map.put(nums[i], i);
-                }
+            Integer lastIndex = map.put(nums[i], i);
+            if (lastIndex != null && (i - lastIndex) <= k) {
+                return true;
             }
         }
         return false;
