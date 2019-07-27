b230e5c00fbeb830fdd2208ca073769fe4700bb5
egbertli <egbert1121@gmail.com>
2019-07-28 04:47:19 +1000

    no brain qs

---
+++ Egbert/Algorithm/Algorithm/src/Array/ContainsDuplicate.java
@@ -0,0 +1,19 @@
+package Array;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!set.add(nums[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ Egbert/AlgorithmTesting/src/Array/ContainsDuplicate.java
@@ -0,0 +1,16 @@
+package Array;
+import java.util.*;
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!set.add(nums[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
