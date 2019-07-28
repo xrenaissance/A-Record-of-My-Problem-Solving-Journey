972a1923921ad583753231e09881968d1865a387
egbertli <egbert1121@gmail.com>
2019-07-29 02:43:47 +1000

    with one stack

---
+++ Egbert/AlgorithmTesting/src/QueueAndStack/MinStackWithOneStack.java
@@ -0,0 +1,42 @@
+package QueueAndStack;
+import java.util.*;
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+public class MinStackWithOneStack {
+    int min = Integer.MIN_VALUE;
+    Deque<Integer> stack;
+    public MinStackWithOneStack() {
+        stack = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        if (stack.isEmpty() || x <= min) {
+            stack.offerLast(min);
+            min = x;
+        }
+        stack.offerLast(x);
+    }
+
+    public void pop() {
+        if (stack.isEmpty()) {
+            return;
+        }
+        if (stack.pollLast() == min) {
+            min = stack.pollLast();
+        }
+    }
+
+    public int top() {
+        return stack.peekLast();
+    }
+
+    public int getMin() {
+        return this.min;
+    }
+}
