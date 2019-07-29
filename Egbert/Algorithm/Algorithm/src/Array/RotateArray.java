ebe3613fba2eb7f50796c024e6a3bf98871046e2
egbertli <egbert1121@gmail.com>
2019-07-30 04:50:46 +1000

    3 steps reverse

---
+++ Egbert/Algorithm/Algorithm/src/Array/RotateArray.java
@@ -0,0 +1,29 @@
+package Array;
+
+/**
+ * @leetcode https://leetcode.com/problems/rotate-array/
+ * @date 29/7/2019
+ * @Time N
+ * @Space 1
+ */
+public class RotateArray {
+    public void rotate(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start++, end--);
+        }
+    }
+    private void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+    }
+}
