6455094a90d84575a68442dcac9f978d85a0635e
egbertli <egbert1121@gmail.com>
2019-07-31 06:05:32 +1000

    one to one projection to same value

---
+++ Egbert/Algorithm/Algorithm/src/StringI/IsomorphicStrings.java
@@ -0,0 +1,23 @@
+package StringI;
+
+/**
+ * @leetcode https://leetcode.com/problems/isomorphic-strings/
+ * @Time n
+ * @Space 256 * 2
+ */
+public class IsomorphicStrings {
+    public boolean isIsomorphic(String s, String t) {
+        int[] projS = new int[256];
+        int[] projT = new int[256];
+        for (int i = 0; i < s.length(); i++) {
+            int indexS = s.charAt(i);
+            int indexT = t.charAt(i);
+            if (projS[indexS] != projT[indexT]) {
+                return false;
+            }
+            projS[indexS] = i + 1;
+            projT[indexT] = i + 1;
+        }
+        return true;
+    }
+}
