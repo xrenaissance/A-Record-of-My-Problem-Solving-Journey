c7d653ddf4410430c30d58a4212b00f728008249
egbertli <egbert1121@gmail.com>
2019-07-30 23:23:12 +1000

    even letters or except one, palindrome, even or odd one

---
+++ Egbert/Algorithm/Algorithm/src/Subsets/LongestPalindrome.java
@@ -0,0 +1,28 @@
+package Subsets;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @leetcode https://leetcode.com/problems/longest-palindrome/
+ * @date 29/7/2019
+ * @Time N
+ * @Space N
+ */
+public class LongestPalindrome {
+    public int longestPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (!set.add(s.charAt(i))) {
+                set.remove(s.charAt(i));
+            }
+        }
+        if (set.size() >= 1) {
+            return s.length() - set.size() + 1;
+        }
+        return s.length() - set.size();
+    }
+}
