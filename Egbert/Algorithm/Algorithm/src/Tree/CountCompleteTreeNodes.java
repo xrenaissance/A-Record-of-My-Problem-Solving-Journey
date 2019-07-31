7e9e2bc34de4ec643a10b95c3b5477b51884013a
egbertli <egbert1121@gmail.com>
2019-08-01 02:36:24 +1000

    bfs

---
+++ Egbert/Algorithm/Algorithm/src/Tree/CountCompleteTreeNodes.java
@@ -0,0 +1,33 @@
+package Tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @leetcode https://leetcode.com/problems/count-complete-tree-nodes/
+ * @Time N
+ * @Space 2 ^ height
+ */
+public class CountCompleteTreeNodes {
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int count = 0;
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            count++;
+            if (node.left != null) {
+                queue.offer(node.left);
+            } else {
+                break;
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+        }
+        return count + queue.size();
+    }
+}
