133e908e20f15d15518f1198b287ff3cc8e0b955
egbertli <egbert1121@gmail.com>
2019-08-02 03:58:21 +1000

    dfs and pruning

---
+++ Egbert/Algorithm/Algorithm/src/Subsets/BinaryWatch.java
@@ -0,0 +1,37 @@
+package Subsets;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @leetcode https://leetcode.com/problems/binary-watch/
+ * @Time 12 * 59
+ * @Space 5
+ */
+public class BinaryWatch {
+    // naive dfs
+    public List<String> readBinaryWatch(int num) {
+        List<String> result = new ArrayList<>();
+        if (num < 0) {
+            return result;
+        }
+        StringBuilder sol = new StringBuilder();
+        for (int i = 0; i < 12; i++) {
+            sol.append(i).append(':');
+            int start = sol.length();
+            for (int j = 0; j < 60; j++) {
+                if (Integer.bitCount(i) + Integer.bitCount(j) == num) {
+                    if (j < 10) {
+                        sol.append(0).append(j);
+                    } else {
+                        sol.append(j);
+                    }
+                    result.add(sol.toString());
+                    sol.setLength(start);
+                }
+            }
+            sol.setLength(0);
+        }
+        return result;
+    }
+}
