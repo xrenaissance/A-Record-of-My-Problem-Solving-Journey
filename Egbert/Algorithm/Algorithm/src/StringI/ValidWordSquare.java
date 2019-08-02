c9eb2bb911fe3c021a5faa24395da855c83084d1
egbertli <egbert1121@gmail.com>
2019-08-02 22:52:06 +1000

    clean and easy to understand code

---
+++ Egbert/Algorithm/Algorithm/src/StringI/ValidWordSquare.java
@@ -0,0 +1,32 @@
+package StringI;
+import java.util.List;
+
+/**
+ * @leetcode https://leetcode.com/problems/valid-word-square/
+ * @Time m * n
+ * @Space m * n
+ */
+public class ValidWordSquare {
+    public boolean validWordSquare(List<String> words) {
+        int rows = words.size();
+        int cols = words.get(0).length();
+        for (int i = 0; i < words.size(); i++) {
+            String word = words.get(i);
+            if (!word.equals(getVerticalString(words, i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+    private String getVerticalString(List<String> words, int col) {
+        StringBuilder sol = new StringBuilder();
+        for (int i = 0; i < words.size(); i++) {
+            String word = words.get(i);
+            if (col < word.length()) {
+                sol.append(word.charAt(col));
+            }
+        }
+        return sol.toString();
+    }
+}
+
