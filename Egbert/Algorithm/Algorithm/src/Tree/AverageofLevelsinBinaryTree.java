cc311b91998ad1536d3a1b84dfb007fb35be1128
egbertli <egbert1121@gmail.com>
2019-08-03 00:04:25 +1000

    bfs

---
+++ Egbert/Algorithm/Algorithm/src/Tree/AverageofLevelsinBinaryTree.java
@@ -0,0 +1,38 @@
+package Tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @leetcode https://leetcode.com/problems/average-of-levels-in-binary-tree/
+ * @Time N
+ * @Space 2 ^ height
+ */
+public class AverageofLevelsinBinaryTree {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            double avg = 0;
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                avg += node.val;
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            result.add(avg / size);
+        }
+        return result;
+    }
+}
