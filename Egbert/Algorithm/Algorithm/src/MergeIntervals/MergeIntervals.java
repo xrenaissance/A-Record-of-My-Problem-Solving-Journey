faf6b441abfc3a008466ae97cad2f2b78d4afada
egbertli <egbert1121@gmail.com>
2019-08-04 03:41:17 +1000

    merge interval

---
+++ Egbert/Algorithm/Algorithm/src/MergeIntervals/MergeIntervals.java
@@ -0,0 +1,39 @@
+package MergeIntervals;
+
+import java.util.Arrays;
+
+/**
+ * @leetcode https://leetcode.com/problems/merge-intervals/
+ * @Time NlogN
+ * @Space N for sorting
+ */
+public class MergeIntervals {
+    public int[][] merge(int[][] intervals) {
+        if (intervals == null || intervals.length == 0) {
+            return intervals;
+        }
+        Arrays.sort(intervals, (a1, a2) -> a1[0] - a2[0]);
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+        int slow = 0;
+        // [0, slow) merged interval
+        // [slow, fast) dump
+        // [fast, rest] explore
+        for (int fast = 1; fast < intervals.length; fast++) {
+            int currStart = intervals[fast][0];
+            int currEnd = intervals[fast][1];
+            if (currStart <= end) {
+                end = Math.max(end, currEnd);
+            } else {
+                intervals[slow][0] = start;
+                intervals[slow][1] = end;
+                slow++;
+                start = currStart;
+                end = currEnd;
+            }
+        }
+        intervals[slow][0] = start;
+        intervals[slow][1] = end;
+        return Arrays.copyOf(intervals, slow + 1);
+    }
+}
