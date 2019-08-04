bfa2787c382b88c0a1b339b3965f07d903e10899
egbertli <egbert1121@gmail.com>
2019-08-05 01:51:35 +1000

    set solution

---
+++ Egbert/Algorithm/Algorithm/src/HashMap/LongestConsecutiveSequence.java
@@ -0,0 +1,41 @@
+package HashMap;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @leetcode https://leetcode.com/problems/longest-consecutive-sequence/
+ * @Time N
+ * @Space D number of dictinct numbers
+ */
+public class LongestConsecutiveSequence {
+
+
+    // 2 * n Time
+    // Space: D = number of distinct numbers
+    public int longestConsecutiveSet(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        int longest = 0;
+        for (int num : nums) {
+            if (set.contains(num)) {
+                set.remove(num);
+                int lower = num - 1;
+                int upper = num + 1;
+                while (set.contains(lower)) {
+                    lower--;
+                }
+                while(set.contains(upper)) {
+                    upper++;
+                }
+                longest = Math.max(longest, upper - lower - 1);
+            }
+        }
+        return longest;
+    }
+}
