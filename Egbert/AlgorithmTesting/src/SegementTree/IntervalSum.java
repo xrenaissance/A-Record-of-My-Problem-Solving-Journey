92f8b4d1815e17f5c23e03a57f297d1278d5fa28
egbertli <egbert1121@gmail.com>
2019-08-11 02:54:52 +1000

    same as add strings

---
+++ Egbert/AlgorithmTesting/src/SegementTree/IntervalSum.java
@@ -0,0 +1,4 @@
+package SegementTree;
+
+public class IntervalSum {
+}
---
+++ Egbert/AlgorithmTesting/src/String/CompareVersionNumbers.java
@@ -0,0 +1,33 @@
+package String;
+
+/**
+ * @leetcode https://leetcode.com/problems/compare-version-numbers/
+ * @Time M + N
+ * @Space M + N
+ */
+public class CompareVersionNumbers {
+    public int compareVersion(String version1, String version2) {
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
+        int i = 0;
+        int j = 0;
+        int num1 = 0;
+        int num2 = 0;
+        while (i < v1.length || j < v2.length) {
+            num1 = 0;
+            num2 = 0;
+            if (i < v1.length) {
+                num1 = Integer.parseInt(v1[i++]);
+            }
+            if (j < v2.length) {
+                num2 = Integer.parseInt(v2[j++]);
+            }
+            if (num1 > num2) {
+                return 1;
+            } else if (num1 < num2) {
+                return -1;
+            }
+        }
+        return 0;
+    }
+}
