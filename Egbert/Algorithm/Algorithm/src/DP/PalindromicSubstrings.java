166b55dcf0ff9a4895062e2e393a58879853d89f
egbertli <egbert1121@gmail.com>
2019-08-12 21:11:15 +1000

    dp

---
+++ Egbert/Algorithm/Algorithm/src/DP/PalindromicSubstrings.java
@@ -0,0 +1,34 @@
+package DP;
+
+/**
+ * @leetcode https://leetcode.com/problems/palindromic-substrings/
+ * @Time DFS: n ^ 2
+ * @space DFS: 1
+ */
+public class PalindromicSubstrings {
+
+
+    // Solution 2: DFS
+    public int countSubstringsDFS(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int total = 0;
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i; j < s.length(); j++) {
+                if (isPalin(s, i, j)) {
+                    total++;
+                }
+            }
+        }
+        return total;
+    }
+    private boolean isPalin(String s, int start, int end) {
+        while (start < end) {
+            if (s.charAt(start++) != s.charAt(end--)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
