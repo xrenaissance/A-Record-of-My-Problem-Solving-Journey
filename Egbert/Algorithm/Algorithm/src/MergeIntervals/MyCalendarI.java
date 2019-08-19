69cce86acc01380ec5ad65c5beeb4d499cc08703
egbertli <egbert1121@gmail.com>
2019-08-19 23:44:09 +1000

    merge interval

---
+++ Egbert/Algorithm/Algorithm/src/MergeIntervals/MyCalendarI.java
@@ -0,0 +1,32 @@
+package MergeIntervals;
+
+import java.util.TreeMap;
+
+/**
+ * @leetcode https://leetcode.com/problems/my-calendar-i/
+ * @Time nlogn
+ * @Space n
+ */
+public class MyCalendarI {
+    TreeMap<Integer, Integer> bookings;
+    public MyCalendarI() {
+        bookings = new TreeMap<>();
+    }
+
+    public boolean book(int start, int end) {
+        if (bookings.size() == 0) {
+            bookings.put(start, end);
+            return true;
+        }
+        Integer lowStart = bookings.floorKey(start);
+        if (lowStart != null && bookings.get(lowStart) > start) {
+            return false;
+        }
+        Integer highStart = bookings.ceilingKey(start);
+        if (highStart != null && highStart < end) {
+            return false;
+        }
+        bookings.put(start, end);
+        return true;
+    }
+}
