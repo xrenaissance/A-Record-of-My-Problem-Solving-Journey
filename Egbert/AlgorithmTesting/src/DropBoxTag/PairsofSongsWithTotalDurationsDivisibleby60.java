43ae345d0fe315b3ff11f8431db1a95bc87f6591
egbertli <egbert1121@gmail.com>
2019-08-23 21:45:58 +1000

    hash

---
+++ Egbert/AlgorithmTesting/src/DropBoxTag/PairsofSongsWithTotalDurationsDivisibleby60.java
@@ -0,0 +1,20 @@
+package DropBoxTag;
+
+/**
+ * @leetcode https://leetcode.com/problems/pairs-of-songs-with-total-durations-divisible-by-60/
+ * @Time N
+ * @Space 60
+ */
+public class PairsofSongsWithTotalDurationsDivisibleby60 {
+    public int numPairsDivisibleBy60(int[] time) {
+        int[] modRecord = new int[60];
+        int total = 0;
+        for (int num : time) {
+            int mod = num % 60;
+            int diff = (60 - mod) % 60;
+            total += modRecord[diff];
+            modRecord[mod]++;
+        }
+        return total;
+    }
+}
