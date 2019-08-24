7b95600c9238366c97186f2fc89efc3a8c45518e
egbertli <egbert1121@gmail.com>
2019-08-24 23:26:34 +1000

    cyclic sort

---
+++ Egbert/Algorithm/Algorithm/src/CyclicSort/CyclicSort.java
@@ -0,0 +1,39 @@
+package CyclicSort;
+
+/**
+ * @leetcode
+ * @Time N
+ * @Space 1
+ */
+public class CyclicSort {
+    public static void sort(int[] nums) {
+        if ( nums == null || nums.length == 0) {
+            return;
+        }
+        int index = 0;
+        while (index < nums.length) {
+            while (nums [index] != index + 1) {
+                swap(nums, index, nums[index] - 1);
+            }
+            index++;
+        }
+    }
+    private static void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+    }
+    public static void main(String[] args) {
+        int[] arr = new int[] { 3, 1, 5, 4, 2 };
+        CyclicSort.sort(arr);
+        for (int num : arr)
+            System.out.print(num + " ");
+        System.out.println();
+
+        arr = new int[] { 2, 6, 4, 3, 1, 5 };
+        CyclicSort.sort(arr);
+        for (int num : arr)
+            System.out.print(num + " ");
+        System.out.println();
+    }
+}
---
+++ Egbert/AlgorithmTesting/src/CyclicSort/FindTheDuplicateNumber.java
@@ -0,0 +1,4 @@
+package CyclicSort;
+
+public class FindTheDuplicateNumber {
+}
