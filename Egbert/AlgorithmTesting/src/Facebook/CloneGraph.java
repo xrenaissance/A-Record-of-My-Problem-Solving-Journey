b72d3f96bc2655cfd65951349534451c457a8077
egbertli <egbert1121@gmail.com>
2019-09-06 02:00:37 +1000

    bfs && dfs

--- Egbert/AlgorithmTesting/src/Facebook/CloneGraph.java
+++ Egbert/AlgorithmTesting/src/Facebook/CloneGraph.java
@@ -2,13 +2,15 @@ package Facebook;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
-
+import java.util.Queue;
+import java.util.LinkedList;
 /**
  * @leetcode https://leetcode.com/problems/clone-graph/
  * @Time N number of nodes
  * @Space average length of neighbors
  */
 public class CloneGraph {
+    // solution1: DFS
     public Node cloneGraph(Node node) {
         Map<Integer, Node> map = new HashMap<>();
         return clone(node, map);
@@ -31,5 +33,33 @@ public class CloneGraph {
         }
 
         return cloneNode;
+    }
+
+    // Solution2: BFS
+    public Node cloneGraphBFS(Node node) {
+        if (node == null) {
+            return null;
+        }
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(node);
+
+        Map<Integer, Node> map = new HashMap<>();
+        map.put(node.val, new Node(node.val, new ArrayList<>()));
+
+        while (!queue.isEmpty()) {
+            Node currNode = queue.poll();
+
+            for (Node neighbor : currNode.neighbors) {
+                if (!map.containsKey(neighbor.val)) {
+                    map.put(neighbor.val, new Node(neighbor.val, new ArrayList<>()));
+                    queue.offer(neighbor);
+                }
+
+                map.get(currNode.val).neighbors.add(map.get(neighbor.val));
+            }
+        }
+
+        return map.get(node.val);
     }
 }
