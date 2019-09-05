489f9f94181dda8b43f32783987253de733c15f5
egbertli <egbert1121@gmail.com>
2019-09-06 01:02:01 +1000

    dfs

--- Egbert/AlgorithmTesting/src/Facebook/CloneGraph.java
+++ Egbert/AlgorithmTesting/src/Facebook/CloneGraph.java
@@ -1,4 +1,35 @@
 package Facebook;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 
+/**
+ * @leetcode https://leetcode.com/problems/clone-graph/
+ * @Time N number of nodes
+ * @Space average length of neighbors
+ */
 public class CloneGraph {
+    public Node cloneGraph(Node node) {
+        Map<Integer, Node> map = new HashMap<>();
+        return clone(node, map);
+    }
+
+    private Node clone(Node currNode, Map<Integer, Node> map) {
+        if (currNode == null) {
+            return null;
+        }
+
+        if (map.containsKey(currNode.val)) {
+            return map.get(currNode.val);
+        }
+
+        Node cloneNode = new Node(currNode.val, new ArrayList<>());
+        map.put(currNode.val, cloneNode);
+
+        for (Node neighbor : currNode.neighbors) {
+            cloneNode.neighbors.add(clone(neighbor, map));
+        }
+
+        return cloneNode;
+    }
 }
