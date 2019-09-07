9709d83447ca30d182fa13459acd729ce6ce253c
egbertli <egbert1121@gmail.com>
2019-09-08 01:45:22 +1000

    stocks

--- Egbert/AlgorithmTesting/src/Facebook/BestTimetoBuyandSellStockII.java
+++ Egbert/AlgorithmTesting/src/Facebook/BestTimetoBuyandSellStockII.java
@@ -1,4 +1,35 @@
 package Facebook;
 
+/**
+ * @leetcode https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+ * @Time N
+ * @Space 1
+ */
 public class BestTimetoBuyandSellStockII {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+
+        int minPrice = 0;
+        int peakPrice = 0;
+        int index = 0;
+        int profit = 0;
+        while (index < prices.length - 1) {
+            while (index < prices.length - 1 && prices[index] >= prices[index + 1]) {
+                index++;
+            }
+            minPrice = prices[index];
+
+            while (index < prices.length - 1 && prices[index] <= prices[index + 1]) {
+                index++;
+            }
+            peakPrice = prices[index];
+
+            profit += peakPrice - minPrice;
+            index++;
+        }
+
+        return profit;
+    }
 }
