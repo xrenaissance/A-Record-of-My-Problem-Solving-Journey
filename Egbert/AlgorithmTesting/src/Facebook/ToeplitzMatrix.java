83fe119faac4d482e7b51b54e371ec51651b0556
egbertli <egbert1121@gmail.com>
2019-09-08 22:32:35 +1000

    matrix

---
+++ Egbert/AlgorithmTesting/src/Facebook/ToeplitzMatrix.java
@@ -0,0 +1,68 @@
+package Facebook;
+
+/**
+ * @leetcode https://leetcode.com/problems/toeplitz-matrix/
+ * @Time M * N
+ * @Space
+ */
+public class ToeplitzMatrix {
+    // clean code
+    public boolean isToeplitzMatrixClean(int[][] matrix) {
+        if (matrix == null || matrix.length == 0) {
+            return true;
+        }
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        for (int i = 0; i < rows - 1; i++) {
+            for (int j = 0; j < cols - 1; j++) {
+                if (matrix[i][j] != matrix[i + 1][j + 1]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    // My initial solution
+    // Time m * n
+    // Space 1
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        if (matrix == null || matrix.length == 0) {
+            return true;
+        }
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        // bottom left
+        for (int i = 0; i < rows; i++) {
+            if (!evalDiag(matrix, i, 0)) {
+                return false;
+            }
+        }
+
+        for (int i = 1; i < cols; i++) {
+            if (!evalDiag(matrix, 0, i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean evalDiag(int[][] matrix, int row, int col) {
+        int nextRow = row + 1;
+        int nextCol = col + 1;
+        if (nextRow < matrix.length && nextCol < matrix[0].length) {
+            if (matrix[row][col] != matrix[nextRow][nextCol]) {
+                return false;
+            } else {
+                return evalDiag(matrix, row + 1, col + 1);
+            }
+        }
+
+        return true;
+    }
+}
