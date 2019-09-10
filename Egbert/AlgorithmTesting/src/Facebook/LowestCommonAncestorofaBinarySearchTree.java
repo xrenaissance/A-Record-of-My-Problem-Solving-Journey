093b5cb05789163f601efdc9e79564655567ec95
egbertli <egbert1121@gmail.com>
2019-09-10 23:47:53 +1000

    lca

--- Egbert/AlgorithmTesting/src/Facebook/LowestCommonAncestorofaBinarySearchTree.java
+++ Egbert/AlgorithmTesting/src/Facebook/LowestCommonAncestorofaBinarySearchTree.java
@@ -6,6 +6,40 @@ package Facebook;
  * @Space height
  */
 public class LowestCommonAncestorofaBinarySearchTree {
+    // solution 1 same as solution2 but iterative way
+    public TreeNode lowestCommonAncestorIterative(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return root;
+        }
+
+        while ((root.val - (long)p.val) * (root.val - (long)q.val) > 0) {
+            root = root.val > p.val ? root.left : root.right;
+        }
+
+        return root;
+    }
+    // Solution 2: if the given tree is bst
+    // just walk down from whole tree's root as long as p and q are
+    // in the same subtree, meaning their values are both smaller or
+    // larger than root's
+    public TreeNode lowestCommonAncestorSolRecursion(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return root;
+        }
+
+        if ((root.val - (long)p.val) * (root.val - (long)q.val) > 0) {
+            root = root.val > p.val ? root.left : root.right;
+            return lowestCommonAncestor(root, p, q);
+        }
+
+        return root;
+    }
+
+   // Solution 1
+    // if one of the nodes is root, so the lca is root
+    // if p and q can be found in different subtree then lca is root
+    // if p and q are in the same subtree, then keep searching
+    // if one of result from left or right is null, then return to non-null value
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null) {
             return null;
