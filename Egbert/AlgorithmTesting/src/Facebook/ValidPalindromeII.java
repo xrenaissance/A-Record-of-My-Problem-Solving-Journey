39720b449b4323a2cdc0291c20e8c4a26b60a926
egbertli <egbert1121@gmail.com>
2019-09-11 17:39:12 +1000

    vp

---
+++ Egbert/AlgorithmTesting/src/Facebook/ValidPalindromeII.java
@@ -0,0 +1,40 @@
+package Facebook;
+
+/**
+ * @leetcode https://leetcode.com/problems/valid-palindrome-ii/
+ * @Time 2 * N worst
+ * @Space 1
+ */
+public class ValidPalindromeII {
+    public boolean validPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return true;
+        }
+
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
